1eadcd46c44b9dc0fcf762fd1198a28af2bdd8cf
Ayahito Saji <ayahito_saji@MacBookPro.local>
2019-08-25 09:11:44 +0900

    created env

--- src/frontend/src/views/Home.vue
+++ src/frontend/src/views/Home.vue
@@ -1,13 +1,3 @@
 <template>
-  <HelloWorld />
+  <div>aaa</div>
 </template>
-
-<script>
-import HelloWorld from '../components/HelloWorld';
-
-export default {
-  components: {
-    HelloWorld,
-  },
-};
-</script>
